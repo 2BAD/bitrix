2d51aadd1c91f91d7650be6d32ec9323b46302d8
Mr. Hyde <github@2bad.me>
2019-07-12 21:31:32 +0300

    [ts] configure `got` instance to make authorized requests to bitrix api

---
+++ source/client.ts
@@ -0,0 +1,33 @@
+// tslint:disable:object-literal-sort-keys
+
+import got from 'got'
+import { name, version } from '~/../package.json'
+
+export const client = (restUri: string, token: string) => {
+  const { get } = got.extend({
+    baseUrl: restUri,
+    headers: {
+      'user-agent': `${name}/${version}`
+    },
+    json: true,
+    hooks: {
+      beforeRequest: [
+        (options) => {
+          const path = (options && options.path) ? options.path : ''
+          // tslint:disable-next-line:no-object-mutation no-expression-statement
+          options.path += (path.indexOf('?') === -1) ? `?access_token=${token}` : `&access_token=${token}`
+        }
+      ]
+      // should be used with rate limiter to handle throttling cases
+      // afterResponse: [
+      //   (response) => {
+      //     return response
+      //   }
+      // ]
+    }
+  })
+
+  return {
+    get: (resource: string, query: string) => get(resource, { query }).then((response) => response.body)
+  }
+}
