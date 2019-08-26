7a1fb260ac8235ef59d692a873b098ce9809c34a
ArmorDarks <git@lavrins.com>
2019-08-26 17:04:23 +0300

    [ts] add `makeBitrixURIParams` test

--- source/bitrix/utils/makeBitrixURIParams.test.ts
+++ source/bitrix/utils/makeBitrixURIParams.test.ts
@@ -1,6 +1,14 @@
 /* eslint-env jest */
 // tslint:disable: no-expression-statement
 
+import makeBitrixURIParams from './makeBitrixURIParams'
+
 describe('`makeBitrixURIParams` method', () => {
-  it.todo('should properly transform command params object into the query part of the Bitrix API URI')
+  it('should transform command params object into the query part of the Bitrix API URI', () => {
+    expect(makeBitrixURIParams({ param1: 1, param2: 'value' })).toMatchSnapshot()
+  })
+
+  it('should return empty string when there is no params', () => {
+    expect(makeBitrixURIParams({})).toMatchSnapshot()
+  })
 })
--- source/bitrix/utils/makeBitrixURIParams.ts
+++ source/bitrix/utils/makeBitrixURIParams.ts
@@ -1,12 +1,7 @@
 import { BitrixCommandParams } from '../types'
 
-export default (params: BitrixCommandParams): string => {
-  const names = Object.keys(params)
-
-  return names.length > 0
-    ? names
-      .reduce((query, name) => `${query}&${name}=${params[name]}`, '')
-      // That will happen only for first instance
-      .replace('&', '')
-    : ''
-}
+export default (params: BitrixCommandParams): string =>
+  Object.keys(params)
+    .reduce((query, name) => `${query}&${name}=${params[name]}`, '')
+    // That will happen only for first instance
+    .replace('&', '')
