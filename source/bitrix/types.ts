5f921ac2f77f3e3a7283289dc19e4c1149ac5fb0
ArmorDarks <git@lavrins.com>
2019-08-28 16:06:27 +0300

    [ts] remove no longer actual todo

--- source/bitrix/types.ts
+++ source/bitrix/types.ts
@@ -1,7 +1,5 @@
 import { Diff } from './utils/Diff'
 
-// @todo We'd want to have gettable and listable as separate enums,
-//       but we can't due too types compatibility issues when `list` uses `get` internally
 export enum BitrixMethod {
   // Gettable
   BATCH = 'batch',
