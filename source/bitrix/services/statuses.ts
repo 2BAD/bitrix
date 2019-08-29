c36b8d3373b98740230e1f2cefe3d72235ddfd11
ArmorDarks <git@lavrins.com>
2019-08-29 22:05:09 +0300

    [ts] use in Statuses service same params format as in others

--- source/bitrix/services/statuses.ts
+++ source/bitrix/services/statuses.ts
@@ -1,21 +1,22 @@
 // tslint:disable:object-literal-sort-keys
 
 import {
+  GetParams,
   GetPayload,
   GettableMethod,
   ListableMethod,
-  ListOptions,
+  ListParams,
   ListPayload,
   Method,
   Status
 } from '../types'
 
 interface Dependencies {
-  readonly get: <P>(method: GettableMethod, query?: object | string) => Promise<GetPayload<P>>
-  readonly list: <P>(method: ListableMethod, options?: ListOptions) => Promise<ListPayload<P>>
+  readonly get: <P>(method: GettableMethod, params?: GetParams) => Promise<GetPayload<P>>
+  readonly list: <P>(method: ListableMethod, params?: ListParams) => Promise<ListPayload<P>>
 }
 
 export default ({ get, list }: Dependencies) => ({
-  get: () => get<Status>(Method.GET_STATUS, {}),
-  list: (options?: ListOptions) => list<Status>(Method.LIST_STATUSES, options)
+  get: (params?: GetParams) => get<Status>(Method.GET_STATUS, params),
+  list: (params?: ListParams) => list<Status>(Method.LIST_STATUSES, params)
 })
