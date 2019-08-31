44be8863f1eecdf53ad888e5e9d53a096b8d6c09
ArmorDarks <git@lavrins.com>
2019-08-31 16:35:35 +0300

    [ts] refactor `List` to work with new `Batch`, which greatly simplifies code:
    1. It no longer requires functions to chunk commands
    2. It no longer have to care about dispatching numerous batch calls, as `Batch` will handle it
    3. It no longer should care about merging multiple batch calls

--- source/client/methods/list.ts
+++ source/client/methods/list.ts
@@ -7,38 +7,27 @@ import {
   ListPayload
 } from '../../types'
 import isArray from '../../utils/isArray'
-import { Batch, MAX_COMMANDS_PER_BATCH } from './batch'
+import { Batch } from './batch'
 import { GetList } from './getList'
 
 const MAX_ENTRIES_PER_COMMAND = 50
-const MAX_ENTRIES_PER_BATCH = MAX_ENTRIES_PER_COMMAND * MAX_COMMANDS_PER_BATCH
 
-const fillWithBatchCommands = ({ method, params }: Command, start: number, toProcess: number): Commands => {
-  const requiresCommands = Math.ceil(toProcess / MAX_ENTRIES_PER_COMMAND)
-  const commandsToDo = requiresCommands > MAX_COMMANDS_PER_BATCH
-    ? MAX_COMMANDS_PER_BATCH
-    : requiresCommands
+const fillWithCommands = (
+  { method, params }: Command,
+  start: number,
+  toProcess: number,
+  entriesPerCommand: number
+): Commands => {
+  const requiresCommands = Math.ceil(toProcess / entriesPerCommand)
 
-  return range(0, commandsToDo).reduce((commands, i) => ({
+  return range(0, requiresCommands).reduce((commands, i) => ({
     ...commands,
-    [i]: { method, params: { ...params, start: start + (MAX_ENTRIES_PER_COMMAND * i) } }
+    [i]: { method, params: { ...params, start: start + (entriesPerCommand * i) } }
   }), {})
-}
-
-const fillWithBatchesCommands = (command: Command, start: number, toProcess: number): readonly Commands[] => {
-  const requiresBatches = Math.ceil(toProcess / MAX_ENTRIES_PER_BATCH)
-
-  return range(0, requiresBatches).reduce((batchesCommands, i) => {
-    const processed = start + (MAX_ENTRIES_PER_BATCH * i)
-    const remained = toProcess - processed
-
-    return [ ...batchesCommands, fillWithBatchCommands(command, processed, remained)]
-  }, [] as readonly Commands[])
 }
 
 interface Dependencies {
   readonly getList: GetList
-  // tslint:disable-next-line no-mixed-interface
   readonly batch: Batch
 }
 
@@ -53,56 +42,29 @@ export default ({ getList, batch }: Dependencies): List => {
     if (!firstCall.next) return firstCall
 
     const toProcess = firstCall.total - start
-    const batches = fillWithBatchesCommands({ method, params }, start, toProcess)
-      .map((commands) => batch<Record<string | number, readonly P[]>>(commands))
+    const batchCommands = fillWithCommands({ method, params }, start, toProcess, MAX_ENTRIES_PER_COMMAND)
 
-    return Promise.all(batches)
+    return batch<Record<string | number, readonly P[]>>(batchCommands)
       // @todo Messy, made in hurry. Refactor this. Had some issues with types
-      .then((batchesResults) => {
-        const flattenBatches = batchesResults.reduce((collectedFlattenBatches, batchesResult) => {
-          const { result, result_error } = batchesResult.result
-          const flattenCommands = isArray(result)
-            ? result.reduce((res, r) => [...res, ...r], [] as readonly P[])
-            : Object.values(result)
-                .reduce((res, r) => {
-                  // tslint:disable-next-line no-if-statement
-                  if (!res || !r) return []
-                  return [...res, ...r]
-                }, [] as readonly P[])
+      .then(({ result: { result, result_error }, time }) => {
+        const flattenResult = isArray(result)
+          ? result.reduce((res, r) => [...res, ...r], [] as readonly P[])
+          : Object.values(result).reduce((res, r) => !res || !r ? [] : [...res, ...r], [] as readonly P[])
 
-          const flattenErrors = isArray(result_error) ? result_error : Object.keys(result_error)
-            // @todo Object.values doesn't work here as it produces strange type
-            .reduce((res, errorName) => {
-              const error = result_error[errorName]
-              return error ? [...res, error] : res
-            }, [] as readonly string[])
-
-          return {
-            ...collectedFlattenBatches,
-            result: [...collectedFlattenBatches.result, ...(flattenCommands ? flattenCommands : [])],
-            result_error: [...(collectedFlattenBatches.result_error || []), ...flattenErrors]
-          }
-        // @todo This is to make it work quick, should be fixed
-        // tslint:disable-next-line: no-object-literal-type-assertion
-        }, { result: [], result_error: [] } as {
-          readonly result: readonly P[], readonly result_error: readonly string[]
-        })
-
-        // tslint:disable-next-line no-if-statement
-        if (flattenBatches.result_error.length > 0) {
+              // tslint:disable-next-line no-if-statement
+        if (result_error.length && result_error.length > 0) {
           // tslint:disable-next-line no-throw
           throw new Error(
-            `[batch] failed to process the batch. Received ${flattenBatches.result_error.length} errors.`
+            `[batch] failed to process the batch. Received ${result_error.length} errors.`
           )
         }
 
         return {
           error: undefined,
-          // @todo
           next: undefined,
-          result: flattenBatches.result,
+          result: flattenResult ? flattenResult : [],
           // @todo Not accurate, we do not care
-          time: firstCall.time,
+          time,
           total: firstCall.total
         }
       })
