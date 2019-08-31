07d3b82202b6a30d6ee3550f6b6be36a6e2f31b6
ArmorDarks <git@lavrins.com>
2019-08-31 16:27:47 +0300

    [ts][package] refactor `Batch` to accept any number of commands and split it in standalone requests if needed and merge payloads afterwards

--- source/client/methods/batch.ts
+++ source/client/methods/batch.ts
@@ -1,9 +1,32 @@
 import { GotInstance, GotJSONFn } from 'got'
+import chunk from 'lodash.chunk'
+import fromPairs from 'lodash.frompairs'
 import { stringify as toQuery } from 'qs'
 import { BatchPayload, Command, Commands, Method } from '../../types'
 import isArray from '../../utils/isArray'
 
 export const MAX_COMMANDS_PER_BATCH = 50
+
+/**
+ * Split any number of commands into the chunks with max commands per batch in each chunk
+ */
+export const chunkCommands = <C extends Commands>(
+  commands: C,
+  size: number = MAX_COMMANDS_PER_BATCH
+): ReadonlyArray<C> => {
+  const commandsIsArray = isArray(commands)
+  const chunks = chunk(Object.entries(commands), size)
+
+  // @todo Generated return type isn't very accurate since it says that it will make array of <C>, while
+  //       in fact <C> will be distributed among chunks. That gives wrong hints with named commands and arrays as const.
+  return chunks.map(
+    (c) => commandsIsArray
+      // @todo Figure out how to avoid unsafe casting to <C> here
+      //       Without `<C>` generic it won't work with complex functions like `batch`
+      ? c.map(([_key, command]) => command) as unknown as C
+      : fromPairs(c) as C
+  )
+}
 
 /**
  * @note We could avoid that function... if only Bitrix API would support posting of the batch commands as
@@ -34,10 +57,37 @@ export const handleBatchPayload = <C>(payload: BatchPayload<C>): BatchPayload<C>
   return payload
 }
 
+/**
+ * Merge list of batch responses into a single batch
+ * @todo Generics inference is complicated here and might be not super accurate
+ */
+export const mergeBatchPayloads = <
+  B extends BatchPayload<any>,
+  P = B extends BatchPayload<infer U> ? U : never
+>(payloads: ReadonlyArray<B>): BatchPayload<P> => {
+  const merge = <T extends S, S extends readonly any[] | Record<string, any>>(a: T, b: S): T =>
+    isArray(a) && isArray(b)
+      // @todo Gotta find way to avoid unsafe casting here
+      ? [...a, ...b] as unknown as T
+      : ({ ...a, ...b })
+
+  return payloads.reduce((merged, payload) => ({
+    result: {
+      result: merge(merged.result.result || [], payload.result.result),
+      result_error: merge(merged.result.result_error || [], payload.result.result_error),
+      result_next: merge(merged.result.result_next || [], payload.result.result_next),
+      result_time: merge(merged.result.result_time || [], payload.result.result_time),
+      result_total: merge(merged.result.result_total || [], payload.result.result_total)
+    },
+    time: { ...merged.time, ...payload.time }
+  // tslint:disable-next-line: no-object-literal-type-assertion
+  }), { result: {}, time: {} } as BatchPayload<P>)
+}
+
 export type Batch = <
   CPM extends { [K in keyof C]: unknown },
   C extends { [K in keyof CPM]: Command} = { [K in keyof CPM]: Command}
->(commands: C) => Promise<BatchPayload<CPM>>
+>(commands: C, commandsPerRequest?: number) => Promise<BatchPayload<CPM>>
 
 export default ({ get }: GotInstance<GotJSONFn>): Batch => {
   /**
@@ -97,19 +147,18 @@ export default ({ get }: GotInstance<GotJSONFn>): Batch => {
   const batch: Batch = async <
     CPM extends { [K in keyof C]: unknown },
     C extends { [K in keyof CPM]: Command } = { [K in keyof CPM]: Command }
-  >(commands: C): Promise<BatchPayload<CPM>> => {
-    const commandsAmount = Object.keys(commands).length
+  >(commands: C, commandsPerRequest: number = MAX_COMMANDS_PER_BATCH): Promise<BatchPayload<CPM>> => {
+    const call = (c: C) => get(Method.BATCH, { query: prepareCommandsQueries(c) })
+      .then(({ body }) => body as BatchPayload<CPM>)
 
-    // tslint:disable-next-line no-if-statement
-    if (commandsAmount > MAX_COMMANDS_PER_BATCH) {
-      // tslint:disable-next-line no-throw
-      throw new Error(
-        `[batch] failed to process. Received ${commandsAmount} commands, but maximum ${MAX_COMMANDS_PER_BATCH} allowed`
-      )
-    }
+    const calls = chunkCommands(commands, commandsPerRequest)
+      .map(call)
 
-    return get(Method.BATCH, { query: prepareCommandsQueries(commands) })
-      .then(({ body }) => handleBatchPayload(body as BatchPayload<CPM>))
+    return Promise.all(calls)
+      .then((chunkedPayloads) => {
+        const payloads = mergeBatchPayloads(chunkedPayloads)
+        return handleBatchPayload(payloads)
+      })
   }
 
   return batch
--- source/client/methods/batch.unit.test.ts
+++ source/client/methods/batch.unit.test.ts
@@ -2,10 +2,42 @@
 // tslint:disable: no-expression-statement
 
 import got from 'got'
-import range from 'lodash.range'
 import nock from 'nock'
 import { Method } from '../../types'
-import Batch, { handleBatchPayload, MAX_COMMANDS_PER_BATCH, prepareCommandsQueries } from './batch'
+import Batch, {
+  chunkCommands,
+  handleBatchPayload,
+  mergeBatchPayloads,
+  prepareCommandsQueries
+} from './batch'
+
+describe('Bitrix `chunkCommands` method', () => {
+  it('should chunk named commands', () => {
+    const chunkSize = 2
+    const commands = {
+      a: { method: Method.GET_DEAL },
+      b: { method: Method.GET_LEAD },
+      c: { method: Method.LIST_DEALS },
+      d: { method: Method.LIST_LEADS },
+      e: { method: Method.LIST_STATUSES }
+    }
+
+    expect(chunkCommands(commands, chunkSize)).toMatchSnapshot()
+  })
+
+  it('should chunk array of commands', () => {
+    const chunkSize = 2
+    const commands = [
+      { method: Method.GET_DEAL },
+      { method: Method.GET_LEAD },
+      { method: Method.LIST_DEALS },
+      { method: Method.LIST_LEADS },
+      { method: Method.LIST_STATUSES }
+    ] as const
+
+    expect(chunkCommands(commands, chunkSize)).toMatchSnapshot()
+  })
+})
 
 describe('Bitrix `prepareCommandsQueries` method', () => {
   it('should transform dict of the commands into the query object', () => {
@@ -119,6 +151,162 @@ const TEST_URI = 'https://test.com/rest'
 const batch = Batch(got.extend({ baseUrl: TEST_URI, json: true }))
 const RESPONSE_200 = 200
 
+describe('Bitrix `mergeBatchPayloads` method', () => {
+  it('should merge named payloads', () => {
+    const batch1 = {
+      result: {
+        result: {
+          a: [{ ID: '1' }, { ID: '2' }],
+          b: [{ ID: '3' }, { ID: '4' }]
+        },
+        result_error: [],
+        result_total: { a: 8, b: 8 },
+        result_next: { a: 2, b: 4 },
+        result_time: {
+          a: {
+            start: 1567196891.008149,
+            finish: 1567196891.022234,
+            duration: 0.014085054397583008,
+            processing: 0.013998985290527344,
+            date_start: '2019-08-30T23:28:11+03:00',
+            date_finish: '2019-08-30T23:28:11+03:00'
+          },
+          b: {
+            start: 1567196891.022316,
+            finish: 1567196891.03225,
+            duration: 0.009933948516845703,
+            processing: 0.009846210479736328,
+            date_start: '2019-08-30T23:28:11+03:00',
+            date_finish: '2019-08-30T23:28:11+03:00'
+          }
+        }
+      },
+      time: {
+        start: 1567196890.959017,
+        finish: 1567196891.223739,
+        duration: 0.2647218704223633,
+        processing: 0.21567082405090332,
+        date_start: '2019-08-30T23:28:10+03:00',
+        date_finish: '2019-08-30T23:28:11+03:00'
+      }
+    }
+
+    const batch2 = {
+      result: {
+        result: {
+          c: [{ ID: '5' }, { ID: '6' }],
+          d: [{ ID: '7' }, { ID: '8' }]
+        },
+        result_error: [],
+        result_total: { c: 8, d: 8 },
+        result_next: { c: 6, d: 8 },
+        result_time: {
+          c: {
+            start: 1567196891.032315,
+            finish: 1567196891.035297,
+            duration: 0.002981901168823242,
+            processing: 0.002897024154663086,
+            date_start: '2019-08-30T23:28:11+03:00',
+            date_finish: '2019-08-30T23:28:11+03:00'
+          },
+          d: {
+            start: 1567196891.03536,
+            finish: 1567196891.039936,
+            duration: 0.004575967788696289,
+            processing: 0.00450897216796875,
+            date_start: '2019-08-30T23:28:11+03:00',
+            date_finish: '2019-08-30T23:28:11+03:00'
+          }
+        }
+      },
+      time: {
+        start: 1567196890.959517,
+        finish: 1567196891.225739,
+        duration: 0.2647218704223683,
+        processing: 0.21567082405090832,
+        date_start: '2019-08-30T23:28:12+03:00',
+        date_finish: '2019-08-30T23:28:13+03:00'
+      }
+    }
+
+    expect(mergeBatchPayloads([batch1, batch2])).toMatchSnapshot()
+  })
+
+  it('should merge array payloads', () => {
+    const batch1 = {
+      result: {
+        result: [
+          [{ ID: '1' }, { ID: '2' }],
+          [{ ID: '3' }, { ID: '4' }]
+        ],
+        result_error: [],
+        result_total: [8, 8],
+        result_next: [2, 4],
+        result_time: [{
+          start: 1567196891.008149,
+          finish: 1567196891.022234,
+          duration: 0.014085054397583008,
+          processing: 0.013998985290527344,
+          date_start: '2019-08-30T23:28:11+03:00',
+          date_finish: '2019-08-30T23:28:11+03:00'
+        }, {
+          start: 1567196891.022316,
+          finish: 1567196891.03225,
+          duration: 0.009933948516845703,
+          processing: 0.009846210479736328,
+          date_start: '2019-08-30T23:28:11+03:00',
+          date_finish: '2019-08-30T23:28:11+03:00'
+        }]
+      },
+      time: {
+        start: 1567196890.959017,
+        finish: 1567196891.223739,
+        duration: 0.2647218704223633,
+        processing: 0.21567082405090332,
+        date_start: '2019-08-30T23:28:10+03:00',
+        date_finish: '2019-08-30T23:28:11+03:00'
+      }
+    }
+
+    const batch2 = {
+      result: {
+        result: [
+          [{ ID: '5' }, { ID: '6' }],
+          [{ ID: '7' }, { ID: '8' }]
+        ],
+        result_error: [],
+        result_total: [8, 8],
+        result_next: [6, 8],
+        result_time: [{
+          start: 1567196891.032315,
+          finish: 1567196891.035297,
+          duration: 0.002981901168823242,
+          processing: 0.002897024154663086,
+          date_start: '2019-08-30T23:28:11+03:00',
+          date_finish: '2019-08-30T23:28:11+03:00'
+        }, {
+          start: 1567196891.03536,
+          finish: 1567196891.039936,
+          duration: 0.004575967788696289,
+          processing: 0.00450897216796875,
+          date_start: '2019-08-30T23:28:11+03:00',
+          date_finish: '2019-08-30T23:28:11+03:00'
+        }]
+      },
+      time: {
+        start: 1567196890.959517,
+        finish: 1567196891.225739,
+        duration: 0.2647218704223683,
+        processing: 0.21567082405090832,
+        date_start: '2019-08-30T23:28:12+03:00',
+        date_finish: '2019-08-30T23:28:13+03:00'
+      }
+    }
+
+    expect(mergeBatchPayloads([batch1, batch2])).toMatchSnapshot()
+  })
+})
+
 describe('Bitrix `batch` method', () => {
   it('should form a proper request', async () => {
     const payload = { result: { result: { one: 'done', two: 'done' }, result_error: [] } }
@@ -150,7 +338,7 @@ describe('Bitrix `batch` method', () => {
     const commands = {
       0: { method: Method.GET_DEAL, params: { ID: dealId } },
       1: { method: Method.LIST_DEALS }
-    }
+    } as const
 
     const scope = nock(TEST_URI)
       // @todo We'd want to use `query` object here as it is much more readable, but nock for some reason
@@ -185,11 +373,80 @@ describe('Bitrix `batch` method', () => {
     expect(scope.done()).toBe(undefined)
   })
 
-  it('should throw when passing more then max allowed per batch commands', () => {
-    const commands = range(0, MAX_COMMANDS_PER_BATCH + 1)
-      .reduce((comms, i) => ({ ...comms, [i]: Method.LIST_DEALS }), {})
+  it('should form a proper requests with more than max allowed commands per batch', async () => {
+    const maxCommandsPerBatch = 1
+    const payload = { result: { result: { one: 'done', two: 'done' }, result_error: [] } }
+    const dealId = 999
 
-    return expect(batch(commands)).rejects.toMatchSnapshot()
+    const commands = {
+      one: { method: Method.GET_DEAL, params: { ID: dealId } },
+      two: { method: Method.LIST_DEALS }
+    }
+
+    const scope = nock(TEST_URI)
+      // @todo We'd want to use `query` object here as it is much more readable, but nock for some reason
+      //       fails to match request when it contains `cmd[someName]`. The issue definitely connected
+      //       to the `[]`, since it does not appear when only one bracket present
+      .get(
+        `/${Method.BATCH}?cmd%5Bone%5D=${commands.one.method}%3FID%3D${dealId}`
+      )
+      .reply(RESPONSE_200, payload)
+      .get(
+        `/${Method.BATCH}?cmd%5Btwo%5D=${commands.two.method}`
+      )
+      .reply(RESPONSE_200, payload)
+
+    await batch(commands, maxCommandsPerBatch)
+
+    expect(scope.done()).toBe(undefined)
+  })
+
+  it('should form a proper request with more than allowed max numbered commands', async () => {
+    const maxCommandsPerBatch = 1
+    const payload = { result: { result: ['done1', 'done2'], result_error: [] } }
+    const dealId = 999
+
+    const commands = {
+      0: { method: Method.GET_DEAL, params: { ID: dealId } },
+      1: { method: Method.LIST_DEALS }
+    } as const
+
+    const scope = nock(TEST_URI)
+      // @todo We'd want to use `query` object here as it is much more readable, but nock for some reason
+      //       fails to match request when it contains `cmd[someName]`. The issue definitely
+      //       connected to the `[]` since it does not appear when only one bracket present
+      .get(`/${Method.BATCH}?cmd%5B0%5D=${commands[0].method}%3FID%3D${dealId}`)
+      .reply(RESPONSE_200, payload)
+      .get(`/${Method.BATCH}?cmd%5B1%5D=${commands[1].method}`)
+      .reply(RESPONSE_200, payload)
+
+    await batch(commands, maxCommandsPerBatch)
+
+    expect(scope.done()).toBe(undefined)
+  })
+
+  it('should form a proper request with more than max allowed array of commands', async () => {
+    const maxCommandsPerBatch = 1
+    const payload = { result: { result: ['done1', 'done2'], result_error: [] } }
+    const dealId = 999
+
+    const commands = [
+      { method: Method.GET_DEAL, params: { ID: dealId } },
+      { method: Method.LIST_DEALS }
+    ] as const
+
+    const scope = nock(TEST_URI)
+      // @todo We'd want to use `query` object here as it is much more readable, but nock for some reason
+      //       fails to match request when it contains `cmd[someName]`. The issue definitely
+      //       connected to the `[]` since it does not appear when only one bracket present
+      .get(`/${Method.BATCH}?cmd%5B0%5D=${commands[0].method}%3FID%3D${dealId}`)
+      .reply(RESPONSE_200, payload)
+      .get(`/${Method.BATCH}?cmd%5B0%5D=${commands[1].method}`)
+      .reply(RESPONSE_200, payload)
+
+    await batch(commands, maxCommandsPerBatch)
+
+    expect(scope.done()).toBe(undefined)
   })
 
   it('should return body as payload', async () => {
@@ -198,7 +455,16 @@ describe('Bitrix `batch` method', () => {
       1: { method: Method.LIST_DEALS }
     }
 
-    const payload = { result: { result: ['done'], result_error: [] } }
+    const payload = {
+      result: {
+        result: ['done'],
+        result_error: [],
+        result_next: [],
+        result_time: [],
+        result_total: []
+      },
+      time: {}
+    }
 
     nock(TEST_URI)
       // @todo We'd want to use `query` object here as it is much more readable, but nock for some reason
@@ -208,6 +474,48 @@ describe('Bitrix `batch` method', () => {
       .reply(RESPONSE_200, payload)
 
     expect(await batch(commands)).toEqual(payload)
+  })
+
+  // @todo Test this also for named and numbered commands
+  it('should merge payloads when more then max allowed commands provided', async () => {
+    const maxAllowedCommands = 1
+    const commands = {
+      0: { method: Method.GET_DEAL },
+      1: { method: Method.LIST_DEALS }
+    }
+
+    const payload1 = {
+      result: {
+        result: ['done1'],
+        result_error: [],
+        result_next: [1],
+        result_time: [],
+        result_total: [2]
+      },
+      time: {}
+    }
+
+    const payload2 = {
+      result: {
+        result: ['done2'],
+        result_error: [],
+        result_next: [2],
+        result_time: [],
+        result_total: [2]
+      },
+      time: {}
+    }
+
+    nock(TEST_URI)
+      // @todo We'd want to use `query` object here as it is much more readable, but nock for some reason
+      //       fails to match request when it contains `cmd[someName]`. The issue definitely connected
+      //       to the `[]`, since it does not appear when only one bracket present
+      .get(`/${Method.BATCH}?cmd%5B0%5D=${commands[0].method}`)
+      .reply(RESPONSE_200, payload1)
+      .get(`/${Method.BATCH}?cmd%5B1%5D=${commands[1].method}`)
+      .reply(RESPONSE_200, payload2)
+
+    expect(await batch(commands, maxAllowedCommands)).toMatchSnapshot()
   })
 
   it.todo('should cast payload to the <P>')
@@ -250,6 +558,29 @@ describe('Bitrix `batch` method', () => {
       0: { method: Method.GET_DEAL },
       1: { method: Method.LIST_DEALS }
     }
+
+    nock(TEST_URI)
+      // @todo We'd want to use `query` object here as it is much more readable, but nock for some reason
+      //       fails to match request when it contains `cmd[someName]`. The issue definitely connected
+      //       to the `[]`, since it does not appear when only one bracket present
+      .get(`/${Method.BATCH}?cmd%5B0%5D=${commands[0].method}&cmd%5B1%5D=${commands[1].method}`)
+      .reply(RESPONSE_200, payload)
+
+    return expect(batch(commands)).rejects.toMatchSnapshot()
+  })
+
+  it('should throw when getting errors in array of commands payload', () => {
+    const payload = {
+      result: {
+        result: ['done'],
+        result_error: ['Exptected error from array of command `batch` 0', 'Exptected error from array of command `batch` 1']
+      }
+    }
+
+    const commands = [
+      { method: Method.GET_DEAL },
+      { method: Method.LIST_DEALS }
+    ]
 
     nock(TEST_URI)
       // @todo We'd want to use `query` object here as it is much more readable, but nock for some reason
