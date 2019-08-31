cc433a242b2eb816b3ed00ff532174e542a8af87
ArmorDarks <git@lavrins.com>
2019-08-31 17:01:57 +0300

    [ts] try to restructure `List` code

--- source/client/methods/list.ts
+++ source/client/methods/list.ts
@@ -35,36 +35,40 @@ export type List = <P>(method: ListableMethod, params?: ListParams) => Promise<L
 export default ({ getList, batch }: Dependencies): List => {
   const list: List = async <P>(method: ListableMethod, params: ListParams = {}): Promise<ListPayload<P>> => {
     const start = params.start || 0
+
+    const listAll = async () => {
+      const toProcess = firstCall.total - start
+      const batchCommands = fillWithCommands({ method, params }, start, toProcess, MAX_ENTRIES_PER_COMMAND)
+
+      const {
+        result: { result, result_error, result_total },
+        time
+      } = await batch<Record<string | number, readonly P[]>>(batchCommands)
+
+            // tslint:disable-next-line no-if-statement
+      if (result_error.length && result_error.length > 0) {
+        // tslint:disable-next-line no-throw
+        throw new Error(
+          `[list] failed to process the list. Received ${result_error.length} errors.`
+        )
+      }
+
+      const flattenResult = Object.entries(result)
+        .reduce((flatten, [_key, r]) => !r ? flatten : [...flatten, ...r], [] as readonly P[])
+
+      return {
+        error: undefined,
+        next: undefined,
+        result: flattenResult ? flattenResult : [],
+        // @todo Not accurate, we do not care
+        time,
+        total: result_total[0] || 0
+      }
+    }
+
     const firstCall = await getList<P>(method, { ...params, start })
 
-    // tslint:disable-next-line no-if-statement
-    if (!firstCall.next) return firstCall
-
-    const toProcess = firstCall.total - start
-    const batchCommands = fillWithCommands({ method, params }, start, toProcess, MAX_ENTRIES_PER_COMMAND)
-
-    return batch<Record<string | number, readonly P[]>>(batchCommands)
-      .then(({ result: { result, result_error }, time }) => {
-        const flattenResult = Object.entries(result)
-          .reduce((flatten, [_key, r]) => !r ? flatten : [...flatten, ...r], [] as readonly P[])
-
-              // tslint:disable-next-line no-if-statement
-        if (result_error.length && result_error.length > 0) {
-          // tslint:disable-next-line no-throw
-          throw new Error(
-            `[list] failed to process the list. Received ${result_error.length} errors.`
-          )
-        }
-
-        return {
-          error: undefined,
-          next: undefined,
-          result: flattenResult ? flattenResult : [],
-          // @todo Not accurate, we do not care
-          time,
-          total: firstCall.total
-        }
-      })
+    return !firstCall.next ? firstCall : listAll()
   }
 
   return list
