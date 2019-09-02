36facd7fdf177ee3f8d16c91f13cec91daff9a70
Mr. Hyde <github@2bad.me>
2019-09-02 13:20:20 +0300

    [type] add currency type

---
+++ source/services/types/currency.ts
@@ -0,0 +1,16 @@
+import { BoolString, ISODate, NumberString } from './common'
+
+export interface User {
+  readonly CURRENCY: string
+  readonly AMOUNT_CNT: NumberString
+  readonly AMOUNT: NumberString
+  readonly SORT: NumberString
+  readonly BASE: BoolString
+  readonly FULL_NAME: string
+  readonly LID: string
+  readonly FORMAT_STRING: string
+  readonly DEC_POINT: string
+  readonly THOUSANDS_SEP: string
+  readonly DECIMALS: NumberString
+  readonly DATE_UPDATE: ISODate
+}
