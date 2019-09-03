5e1afdc72f1091116e29ae82c25ccbdc969524bd
Mr. Hyde <github@2bad.me>
2019-09-03 23:17:44 +0300

    [ts] add `contscts` methods to entry point

--- source/bitrix.ts
+++ source/bitrix.ts
@@ -1,6 +1,7 @@
 // tslint:disable:object-literal-sort-keys
 
 import Client from './client'
+import ContactsService from './services/contacts'
 import DealsService from './services/deals'
 import LeadsService from './services/leads'
 import StatusesService from './services/statuses'
@@ -13,6 +14,7 @@ export default (restUri: string, accessToken: string) => {
     getList,
     batch,
     list,
+    contacts: ContactsService({ get, list }),
     deals: DealsService({ get, list }),
     leads: LeadsService({ get, list }),
     statuses: StatusesService({ get, list })
