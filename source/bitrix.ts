858423f6899e7d840586ddc1370281344e2dd3c4
ArmorDarks <git@lavrins.com>
2019-09-04 14:14:43 +0300

    [ts] use `call` in Bitrix client

--- source/bitrix.ts
+++ source/bitrix.ts
@@ -7,16 +7,15 @@ import LeadsService from './services/leads'
 import StatusesService from './services/statuses'
 
 export default (restUri: string, accessToken: string) => {
-  const { get, getList, batch, list } = Client(restUri, accessToken)
+  const { call, batch, list } = Client(restUri, accessToken)
 
   return {
-    get,
-    getList,
+    call,
     batch,
     list,
-    contacts: ContactsService({ get, list }),
-    deals: DealsService({ get, list }),
-    leads: LeadsService({ get, list }),
-    statuses: StatusesService({ get, list })
+    contacts: ContactsService({ call, list }),
+    deals: DealsService({ call, list }),
+    leads: LeadsService({ call, list }),
+    statuses: StatusesService({ call, list })
   }
 }
