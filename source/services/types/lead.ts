7942e43d3680a21ae324f08cf1603ca8f35812bd
Mr. Hyde <github@2bad.me>
2019-09-05 22:46:04 +0300

    [type]update `Lead` type

--- source/services/types/lead.ts
+++ source/services/types/lead.ts
@@ -1,28 +1,25 @@
 import { BoolString, ISODate, NumberString } from './common'
 
-// @todo This is approximate structure. Might not be accurate
 export interface Lead {
   // Deal can have user fields
-  // @todo Remove `unknown` when all fields will be known
-  readonly [key: string]: string | unknown
+  readonly [key: string]: string
 
-  // tslint:disable-next-line: no-mixed-interface
   readonly ID: NumberString
   readonly TITLE: string
-  readonly HONORIFIC: unknown | null // @todo
+  readonly HONORIFIC: string | null
   readonly NAME: string | null
   readonly SECOND_NAME: string | null
   readonly LAST_NAME: string | null
   readonly COMPANY_TITLE: string | null
-  readonly COMPANY_ID: unknown | null // @todo
-  readonly CONTACT_ID: unknown | null // @todo
+  readonly COMPANY_ID: NumberString | null
+  readonly CONTACT_ID: NumberString | null
   readonly IS_RETURN_CUSTOMER: BoolString
-  readonly BIRTHDATE: unknown | null // @todo
+  readonly BIRTHDATE: ISODate
   readonly SOURCE_ID: NumberString
-  readonly SOURCE_DESCRIPTION: null
-  readonly STATUS_ID: unknown | null // @todo
+  readonly SOURCE_DESCRIPTION: string | null
+  readonly STATUS_ID: string
   readonly STATUS_DESCRIPTION: string | null
-  readonly POST: unknown | null // @todo
+  readonly POST: string | null
   readonly COMMENTS: string | null
   readonly CURRENCY_ID: string
   readonly OPPORTUNITY: NumberString
@@ -37,8 +34,8 @@ export interface Lead {
   readonly DATE_CLOSED: ISODate
   readonly STATUS_SEMANTIC_ID: string
   readonly OPENED: BoolString
-  readonly ORIGINATOR_ID: unknown | null // @todo
-  readonly ORIGIN_ID: unknown | null // @todo
+  readonly ORIGINATOR_ID: string | null
+  readonly ORIGIN_ID: NumberString | null
   readonly ADDRESS: string | null
   readonly ADDRESS_2: string | null
   readonly ADDRESS_CITY: string | null
