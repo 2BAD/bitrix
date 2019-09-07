ff041908639ceda6af3e55d919f3f3930df7847d
Mr. Hyde <github@2bad.me>
2019-09-07 23:11:46 +0300

    [test] add `statuses` service tests

---
+++ source/services/statuses/index.unit.test.ts
@@ -0,0 +1,68 @@
+/* tslint-env jest */
+// tslint:disable: no-expression-statement object-literal-sort-keys
+
+import Statuses from '.'
+
+const mockCall = jest.fn(() => Promise.resolve()) as any
+const mockList = jest.fn(() => Promise.resolve()) as any
+const statuses = Statuses({ call: mockCall, list: mockList })
+
+const ID = '77'
+
+describe('Statuses', () => {
+  beforeEach(() => {
+    mockCall.mockClear()
+    mockList.mockClear()
+  })
+
+  it('should expose API methods', () => {
+    expect(statuses).toMatchSnapshot()
+  })
+
+  describe('`create`', () => {
+    it('should invoke `call`', async () => {
+      await statuses.create({
+        NAME: 'Test name'
+      })
+      expect(mockCall.mock.calls).toMatchSnapshot()
+    })
+  })
+
+  describe('`get`', () => {
+    it('should invoke `call`', async () => {
+      await statuses.get(ID)
+      expect(mockCall.mock.calls).toMatchSnapshot()
+    })
+  })
+
+  describe('`list`', () => {
+    it('should invoke `list`', async () => {
+      await statuses.list({
+        order: { NAME: 'ASC' },
+        filter: { ENTITY_ID: 'STATUS' }
+      })
+      expect(mockCall.mock.calls).toMatchSnapshot()
+    })
+
+    it('should work without optional params', async () => {
+      await statuses.list()
+      expect(mockCall.mock.calls).toMatchSnapshot()
+    })
+  })
+
+  describe('`update`', () => {
+    it('should invoke `call`', async () => {
+      await statuses.update(ID, {
+        NAME: 'Test name'
+      })
+      expect(mockCall.mock.calls).toMatchSnapshot()
+    })
+
+    it('should work without optional params', async () => {
+      await statuses.update(ID, {
+        NAME: 'Test name'
+      })
+      expect(mockCall.mock.calls).toMatchSnapshot()
+    })
+  })
+})
