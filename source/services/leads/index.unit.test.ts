a2f7a790f8192688294d48c318048004c47b4643
Mr. Hyde <github@2bad.me>
2019-09-07 21:55:18 +0300

    [test] add tests for `leads` service

---
+++ source/services/leads/index.unit.test.ts
@@ -0,0 +1,81 @@
+/* tslint-env jest */
+// tslint:disable: no-expression-statement object-literal-sort-keys
+
+import Leads from '.'
+
+const mockCall = jest.fn(() => Promise.resolve()) as any
+const mockList = jest.fn(() => Promise.resolve()) as any
+const leads = Leads({ call: mockCall, list: mockList })
+
+const ID = '77'
+
+describe('Leads', () => {
+  beforeEach(() => {
+    mockCall.mockClear()
+    mockList.mockClear()
+  })
+
+  it('should expose API methods', () => {
+    expect(leads).toMatchSnapshot()
+  })
+
+  describe('`create`', () => {
+    it('should invoke `call`', async () => {
+      await leads.create({
+        COMMENTS: 'Some comment'
+      }, {
+        REGISTER_SONET_EVENT: 'Y'
+      })
+      expect(mockCall.mock.calls).toMatchSnapshot()
+    })
+
+    it('should work without optional params', async () => {
+      await leads.create({
+        COMMENTS: 'Some comment'
+      })
+      expect(mockCall.mock.calls).toMatchSnapshot()
+    })
+  })
+
+  describe('`get`', () => {
+    it('should invoke `call`', async () => {
+      await leads.get(ID)
+      expect(mockCall.mock.calls).toMatchSnapshot()
+    })
+  })
+
+  describe('`list`', () => {
+    it('should invoke `list`', async () => {
+      await leads.list({
+        start: 0,
+        order: { COMMENTS: 'ASC' },
+        filter: { '>PROBABILITY': 50 },
+        select: ['*', 'UF_*']
+      })
+      expect(mockCall.mock.calls).toMatchSnapshot()
+    })
+
+    it('should work without optional params', async () => {
+      await leads.list()
+      expect(mockCall.mock.calls).toMatchSnapshot()
+    })
+  })
+
+  describe('`update`', () => {
+    it('should invoke `call`', async () => {
+      await leads.update(ID, {
+        COMMENTS: 'Some comment'
+      }, {
+        REGISTER_SONET_EVENT: 'Y'
+      })
+      expect(mockCall.mock.calls).toMatchSnapshot()
+    })
+
+    it('should work without optional params', async () => {
+      await leads.update(ID, {
+        COMMENTS: 'Some comment'
+      })
+      expect(mockCall.mock.calls).toMatchSnapshot()
+    })
+  })
+})
