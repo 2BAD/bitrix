b2498235e74ad6a6d337db50780f043231b5b59f
Mr. Hyde <github@2bad.me>
2019-09-07 21:43:33 +0300

    [test] add `deals` service tests

---
+++ source/services/deals/index.unit.test.ts
@@ -0,0 +1,81 @@
+/* tslint-env jest */
+// tslint:disable: no-expression-statement object-literal-sort-keys
+
+import Deals from '.'
+
+const mockCall = jest.fn(() => Promise.resolve()) as any
+const mockList = jest.fn(() => Promise.resolve()) as any
+const deals = Deals({ call: mockCall, list: mockList })
+
+const ID = '77'
+
+describe('Deals', () => {
+  beforeEach(() => {
+    mockCall.mockClear()
+    mockList.mockClear()
+  })
+
+  it('should expose API methods', () => {
+    expect(deals).toMatchSnapshot()
+  })
+
+  describe('`create`', () => {
+    it('should invoke `call`', async () => {
+      await deals.create({
+        COMMENTS: 'Some comment'
+      }, {
+        REGISTER_SONET_EVENT: 'Y'
+      })
+      expect(mockCall.mock.calls).toMatchSnapshot()
+    })
+
+    it('should work without optional params', async () => {
+      await deals.create({
+        COMMENTS: 'Some comment'
+      })
+      expect(mockCall.mock.calls).toMatchSnapshot()
+    })
+  })
+
+  describe('`get`', () => {
+    it('should invoke `call`', async () => {
+      await deals.get(ID)
+      expect(mockCall.mock.calls).toMatchSnapshot()
+    })
+  })
+
+  describe('`list`', () => {
+    it('should invoke `list`', async () => {
+      await deals.list({
+        start: 0,
+        order: { COMMENTS: 'ASC' },
+        filter: { '>PROBABILITY': 50 },
+        select: ['*', 'UF_*']
+      })
+      expect(mockCall.mock.calls).toMatchSnapshot()
+    })
+
+    it('should work without optional params', async () => {
+      await deals.list()
+      expect(mockCall.mock.calls).toMatchSnapshot()
+    })
+  })
+
+  describe('`update`', () => {
+    it('should invoke `call`', async () => {
+      await deals.update(ID, {
+        COMMENTS: 'Some comment'
+      }, {
+        REGISTER_SONET_EVENT: 'Y'
+      })
+      expect(mockCall.mock.calls).toMatchSnapshot()
+    })
+
+    it('should work without optional params', async () => {
+      await deals.update(ID, {
+        COMMENTS: 'Some comment'
+      })
+      expect(mockCall.mock.calls).toMatchSnapshot()
+    })
+  })
+})
