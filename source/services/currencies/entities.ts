18399332399e4466fc5c4b16529bb3a8883a4971
Mr. Hyde <github@2bad.me>
2019-09-09 05:35:36 +0300

    [misc] fix path

--- source/services/currencies/entities.ts
+++ source/services/currencies/entities.ts
@@ -1,4 +1,4 @@
-import { BoolString, ISODate, NumberString } from './common'
+import { BoolString, ISODate, NumberString } from '../../entities/common'
 
 export interface Currency {
   readonly CURRENCY: string
